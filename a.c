84d2eaf7c075c6c2c9c7cf1d0a0c5e944f82d31e
jleal <jcleal91@gmail.com>
2020-10-10 13:39:08 -0500

    more directory restructuring

--- a.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-// Never include b.c, b.h if you must. If you include, you will drop all the contents of b.c here 
-
-int main()
-{
-    extern char B_VAR[];
-    printf("%s", B_VAR);
-    return 0;
-}
--- b.c
+++
@@ -1,2 +0,0 @@
-// If you add static here, B_VAR is no longer accessible.
-char B_VAR[] = "Hello world!\n";
